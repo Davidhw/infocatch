b9ccc60ba45beeb8e8ec51a27daa4ef9602347fd
David Weinstein <David.Weinstein@ncf.edu>
2015-07-30 20:40:17 -0400

    making debug false which will change base url value, which may fix ajax post on gwern's page

--- rssplus/settings.py
+++ rssplus/settings.py
@@ -34,7 +34,7 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 # SECURITY WARNING: keep the secrte key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 if DEBUG:
     BASE_URL = "test1.com"
 else:
