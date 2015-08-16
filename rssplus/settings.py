7360666c3b0ef40ea3b1ddaa66716fd32bb140cf
David Weinstein <David.Weinstein@ncf.edu>
2015-08-16 14:38:14 -0400

    making debug true

--- rssplus/settings.py
+++ rssplus/settings.py
@@ -35,11 +35,15 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 # SECURITY WARNING: keep the secrte key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
+'''
 DEBUG = False
 if DEBUG:
     BASE_URL = "test1.com"
 else:
     BASE_URL = "https://infocatch.herokuapp.com/"
+'''
+DEBUG = True
+BASE_URL = "https://infocatch.herokuapp.com/"
 
 TEMPLATE_DEBUG = DEBUG
 
